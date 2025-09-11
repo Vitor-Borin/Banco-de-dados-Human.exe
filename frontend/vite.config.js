626cc0b55838e319c299a62faa9fb7637978c5f0
Vitor Borin <vitor.borin06@gmail.com>
2025-09-10 22:22:57 -0300

    Simplifica configurações do Vercel para resolver tela branca

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  base: './',
+    outDir: 'dist'
+  }
 })
